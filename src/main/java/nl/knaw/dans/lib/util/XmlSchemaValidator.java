2e3cf78da0553c968650047589f6fb52252f6947
Jan van Mansum <janvanmansum@gmail.com>
2025-11-14 11:59:11 +0100

    Refactored XmlSchemaValidator

--- src/main/java/nl/knaw/dans/lib/util/XmlSchemaValidator.java
+++ src/main/java/nl/knaw/dans/lib/util/XmlSchemaValidator.java
@@ -15,16 +15,149 @@
  */
 package nl.knaw.dans.lib.util;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.w3c.dom.Node;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public interface XmlSchemaValidator {
+/**
+ * Container that keeps a number of configured XML Schema validators. Each validator has an alias. While it is possible to use the full namespace URI as alias, this is not required. For example, the
+ * alias could be a simple name such as "ddm" or "emd".
+ */
+@Slf4j
+public class XmlSchemaValidator {
+    private final Map<String, URI> aliasToSchemaLocation;
 
-    List<SAXParseException> validateDocument(Node node, String schema) throws IOException, SAXException;
+    protected final Map<String, Schema> schemaMap = new HashMap<>();
+    private final SchemaFactory schemaFactory;
+    private final boolean failOnWarning;
 
-    void loadSchemaInstances() throws Exception;
+    /**
+     * Constructs an XmlSchemaValidator that can validate XML documents against multiple XML Schemas.
+     *
+     * @param aliasToSchemaLocation a map of schema aliases to schema locations (the URL of the XSD, not the namespace)
+     */
+    public XmlSchemaValidator(@NonNull Map<String, URI> aliasToSchemaLocation, boolean failOnWarning) {
+        this.aliasToSchemaLocation = aliasToSchemaLocation;
+        this.schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        this.failOnWarning = failOnWarning;
+        this.loadSchemaInstances();
+    }
+
+    /**
+     * Constructs an XmlSchemaValidator that can validate XML documents against multiple XML Schemas.
+     *
+     * @param aliasToSchemaLocation a map of schema aliases to schema locations (the URL of the XSD, not the namespace)
+     */
+    public XmlSchemaValidator(Map<String, URI> aliasToSchemaLocation) {
+        this(aliasToSchemaLocation, false);
+    }
+
+    private void loadSchemaInstances() {
+        for (var alias : aliasToSchemaLocation.keySet()) {
+            log.debug("Start loading of schema instance for '{}'", alias);
+
+            try {
+                var schemaLocation = aliasToSchemaLocation.get(alias);
+
+                if (schemaLocation != null) {
+                    log.debug("Schema location for {} is: '{}'", alias, schemaLocation);
+                    var schemaInstance = schemaFactory.newSchema(new URL(schemaLocation.toASCIIString()));
+                    log.debug("Storing schema instance for '{}'", alias);
+                    schemaMap.put(alias, schemaInstance);
+                }
+                else {
+                    throw new IllegalArgumentException(String.format("Schema location for alias '%s' is null.", alias));
+                }
+                log.info("Schema instance for '{}' loaded.", alias);
+            }
+            catch (MalformedURLException | SAXException e) {
+                // MalformedURLException should not occur, as URIs are converted to URLs above
+                throw new RuntimeException("Could not load schema for alias '" + alias + "': " + e.getMessage(), e);
+            }
+        }
+    }
+
+    /**
+     * Validates the document loaded into <code>node</code> against the schema mapped to <code>schemaAlias</code>. Note that a more complete validation can be performed by using a Source instead of a
+     * Node, as fatal errors will already be raised when parsing the text into a DOM tree.
+     *
+     * @param node        the XML document to validate
+     * @param schemaAlias the alias of the schema to validate against
+     * @return a list of SAXParseExceptions encountered during validation; an empty list indicates the document is valid
+     * @throws IOException  if the schema cannot be read
+     * @throws SAXException if validation could not be performed
+     */
+    public List<SAXParseException> validateDocument(Node node, String schemaAlias) throws IOException, SAXException {
+        return validateDocument(new DOMSource(node), schemaAlias);
+    }
+
+    /**
+     * Validates the document loaded into <code>node</code> against the schema mapped to <code>schemaAlias</code>.
+     *
+     * @param source      the XML document to validate
+     * @param schemaAlias the alias of the schema to validate against
+     * @return a list of SAXParseExceptions encountered during validation; an empty list indicates the document is valid
+     * @throws IOException  if the schema cannot be read
+     * @throws SAXException if validation could not be performed
+     */
+    public List<SAXParseException> validateDocument(Source source, String schemaAlias) throws IOException, SAXException {
+        var schemaInstance = schemaMap.get(schemaAlias);
+
+        if (schemaInstance == null) {
+            throw new IllegalStateException(String.format("No schema instance found for alias '%s'", schemaAlias));
+        }
+
+        var validator = schemaInstance.newValidator();
+        log.debug("Creating validator for schema alias '{}'", schemaAlias);
+        var exceptions = new ArrayList<SAXParseException>();
+
+        validator.setErrorHandler(new ErrorHandler() {
+
+            @Override
+            public void warning(SAXParseException e) {
+                // Unsure how to test this; what constitutes a warning seems to be implementation-dependent
+                if (failOnWarning) {
+                    exceptions.add(e);
+                }
+            }
+
+            @Override
+            public void error(SAXParseException e) {
+                exceptions.add(e);
+            }
+
+            @Override
+            public void fatalError(SAXParseException e) {
+                exceptions.add(e);
+            }
+        });
+
+        log.debug("Validating document against schema alias '{}'", schemaAlias);
+        try {
+            validator.validate(source);
+        }
+        catch (SAXException e) {
+            log.debug("Caught SAXException during validation: {}. It should be collected via the ErrorHandler, so we ignore it here.", e.getMessage());
+        }
+
+        log.debug("Validation against schema alias '{}' completed with {} exceptions", schemaAlias, exceptions.size());
+        return exceptions;
+    }
+
 }
---
+++ src/test/java/nl/knaw/dans/lib/util/XmlSchemaValidatorTest.java
@@ -0,0 +1,246 @@
+package nl.knaw.dans.lib.util;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class XmlSchemaValidatorTest {
+    private DocumentBuilderFactory getFactory() throws ParserConfigurationException {
+        var factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setNamespaceAware(true);
+        return factory;
+    }
+
+    @Test
+    public void should_validate_conforming_standalone_document() throws Exception {
+        // Given
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI(),
+            "note", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/note.xsd")).toURI(),
+            "imported", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/imported.xsd")).toURI()
+        );
+        var personXml = """
+            <person xmlns="http://example.com/person">
+                <name>John Doe</name>
+                <age>30</age>
+            </person>
+            """;
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(new StreamSource(new StringReader(personXml)), "person");
+
+        assertThat(exceptions).isEmpty();
+    }
+
+    @Test
+    public void should_validate_conforming_perparsed_standalone_document() throws Exception {
+        // Given
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI(),
+            "note", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/note.xsd")).toURI(),
+            "imported", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/imported.xsd")).toURI()
+        );
+        var personXml = """
+            <person xmlns="http://example.com/person">
+                <name>John Doe</name>
+                <age>30</age>
+            </person>
+            """;
+        var personDocument = getFactory().newDocumentBuilder()
+            .parse(new java.io.ByteArrayInputStream(personXml.getBytes(java.nio.charset.StandardCharsets.UTF_8)));
+
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(personDocument.getDocumentElement(), "person");
+
+        assertThat(exceptions).isEmpty();
+    }
+
+    @Test
+    public void should_validate_conforming_document_with_import() throws Exception {
+        // Given
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI(),
+            "note", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/note.xsd")).toURI(),
+            "message", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/imported.xsd")).toURI()
+        );
+        var messageXml = """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <message xmlns="http://example.com/message"
+                     xmlns:p="http://example.com/person"
+                     xmlns:n="http://example.com/note">
+              <from>
+                <p:person>
+                  <p:name>John Doe</p:name>
+                  <p:age>30</p:age>
+                </p:person>
+              </from>
+              <to>
+                <p:person>
+                  <p:name>Jane Smith</p:name>
+                  <p:age>28</p:age>
+                </p:person>
+              </to>
+              <n:note>Hello, this is a note.</n:note>
+            </message>
+            
+            """;
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(new StreamSource(new StringReader(messageXml)), "message");
+        assertThat(exceptions).isEmpty();
+    }
+
+    @Test
+    public void should_report_error_for_missing_required_element() throws Exception {
+        // Given: missing <from> element
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI(),
+            "note", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/note.xsd")).toURI(),
+            "message", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/imported.xsd")).toURI()
+        );
+        var invalidXml = """
+            <message xmlns="http://example.com/message"
+                     xmlns:p="http://example.com/person"
+                     xmlns:n="http://example.com/note">
+              <to>
+                <p:person>
+                  <p:name>Jane Smith</p:name>
+                  <p:age>28</p:age>
+                </p:person>
+              </to>
+              <n:note>Missing from element</n:note>
+            </message>
+            """;
+
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(new StreamSource(new StringReader(invalidXml)), "message");
+
+        // Then
+        assertThat(exceptions).isNotEmpty();
+        assertThat(exceptions.get(0).getMessage()).contains(
+            "cvc-complex-type.2.4.a: Invalid content was found starting with element '{\"http://example.com/message\":to}'. One of '{\"http://example.com/message\":from}' is expected.");
+    }
+
+    @Test
+    public void should_report_error_for_wrong_namespace() throws Exception {
+        // Given: <person> element in default namespace instead of person namespace
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI(),
+            "note", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/note.xsd")).toURI(),
+            "message", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/imported.xsd")).toURI()
+        );
+        var invalidXml = """
+            <message xmlns="http://example.com/message"
+                     xmlns:p="http://example.com/person"
+                     xmlns:n="http://example.com/note">
+              <from>
+                <person>
+                  <name>John Doe</name>
+                  <age>30</age>
+                </person>
+              </from>
+              <to>cvc-complex-type.2.4.a: Invalid content was found starting with element '{"http://example.com/message":person}'. One of '{"http://example.com/person":person}' is expected.
+                <p:person>
+                  <p:name>Jane Smith</p:name>
+                  <p:age>28</p:age>
+                </p:person>
+              </to>
+              <n:note>Namespace error</n:note>
+            </message>
+            """;
+
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(new StreamSource(new StringReader(invalidXml)), "message");
+
+        // Then
+        assertThat(exceptions).isNotEmpty();
+        assertThat(exceptions.get(0).getMessage()).contains(
+            "cvc-complex-type.2.4.a: Invalid content was found starting with element '{\"http://example.com/message\":person}'. One of '{\"http://example.com/person\":person}' is expected.");
+    }
+
+    // An unknown alias should result in an IllegalStateException
+    @Test
+    public void should_throw_exception_for_unknown_schema_alias() throws Exception {
+        // Given
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI()
+        );
+        var personXml = """
+            <person xmlns="http://example.com/person">
+                <name>John Doe</name>
+                <age>30</age>
+            </person>
+            """;
+
+        // When / Then
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        assertThatThrownBy(() -> validator.validateDocument(new StreamSource(new StringReader(personXml)), "unknown"))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessageContaining("No schema instance found for alias 'unknown'");
+    }
+
+    @Test
+    public void should_report_fatal_error_as_an_error() throws Exception {
+        // Given: an XML with an extra, undefined attribute (should trigger a warning)
+        Map<String, URI> aliasToSchemaLocation = Map.of(
+            "person", Objects.requireNonNull(XmlSchemaValidatorTest.class.getResource("/XmlSchemaValidatorTest/person.xsd")).toURI()
+        );
+        var personXml = """
+            <person xmlns="http://example.com/person">
+                <name>John Doe</name>
+                <age>30</age>
+            </UNMATCHED_TAG>
+            """;
+
+        // When
+        var validator = new XmlSchemaValidator(aliasToSchemaLocation);
+        var exceptions = validator.validateDocument(new StreamSource(new StringReader(personXml)), "person");
+
+        // Then: the UNMATCHED_TAG should cause a fatal error
+        assertThat(exceptions).isNotEmpty();
+        assertThat(exceptions.get(0).getMessage()).contains("The element type \"person\" must be terminated by the matching end-tag \"</person>\".");
+    }
+
+    @Test
+    public void should_throw_illegal_argument_exception_for_null_schema_map() {
+        assertThatThrownBy(() -> new XmlSchemaValidator(null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessageContaining("aliasToSchemaLocation is marked non-null but is null");
+    }
+
+    @Test
+    public void should_throw_illegal_argument_exception_for_null_schema_location() {
+        Map<String, URI> aliasToSchemaLocation = new HashMap<>();
+        aliasToSchemaLocation.put("person", null);
+        assertThatThrownBy(() -> new XmlSchemaValidator(aliasToSchemaLocation))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Schema location for alias 'person' is null");
+    }
+
+    @Test
+    public void should_throw_runtime_exception_for_invalid_schema() {
+        Map<String, URI> aliasToSchemaLocation = new HashMap<>();
+        aliasToSchemaLocation.put("invalid", URI.create("http://example.com/invalid.xsd"));
+        assertThatThrownBy(() -> new XmlSchemaValidator(aliasToSchemaLocation))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("Could not load schema for alias 'invalid'");
+        // The actual problem with the schema is not contained in the message, so not checked here
+    }
+
+}
